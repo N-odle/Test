4c6ae740550ee3ffb4945d36f8115d5f642a7537
N-odle <joshtiger81@gmail.com>
2022-09-13 13:51:25 +0000

    Reeee

---
+++ sendToW/cproject/2808ICT:7623ICT-C/HangMan/obj/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "dictio.h"
+#include "game.h"
+
+int main() {
+    char c [1];
+    run(1);
+    printf("\n\nPress a key to exit.\n");
+    scanf("%c", c);
+    return 0;
+}
